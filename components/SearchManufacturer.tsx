ad39b4525957aa9522c73c0f03ead3a62604b263
DevDan <ogunniyioreyemi@gmail.com>
2023-09-26 22:05:32 +0100

    initial commit

---
+++ components/SearchManufacturer.tsx
@@ -0,0 +1,10 @@
+import { Combobox, Transition } from '@headlessui/react'
+import { SearchManufacturerProps } from '@/types'
+
+const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacturerProps) => {
+  return (
+    <div>SearchManufacturer</div>
+  )
+}
+
+export default SearchManufacturer
